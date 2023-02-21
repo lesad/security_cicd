e2a6398b6ea2cf885c94fc5ee610105b607e85ad
lesad <70895092+lesad@users.noreply.github.com>
2023-02-21 14:15:04 +0100

    I commit, you fix

---
+++ code.py
@@ -0,0 +1,12 @@
+def main():
+    # craete a list of 10 random numbers
+    list1 = [random.randint(0, 100) for i in range(10)]
+
+    # do some erronous things
+    x = list1[300]
+
+    with open("test.txt", "r") as f:
+        f.write("Hello World")
+
+if __name__ == "__main__":
+    pass
